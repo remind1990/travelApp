657141916dc55fb1cc85366e7707fde5ed877068
remind1990 <tigerstylest00@gmail.com>
2023-10-18 17:16:45 -0230

    smaller marker

--- src/ui/customMarker.js
+++ src/ui/customMarker.js
@@ -2,7 +2,7 @@ import L from 'leaflet';
 
 const iconMarker = L.icon({
   iconUrl: '/pass.svg',
-  iconSize: [40, 60], // size of the icon
+  iconSize: [30, 50], // size of the icon
   popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
 });
 
