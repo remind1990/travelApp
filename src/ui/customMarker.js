bb7f7fb7e58d398497cbb8f888ead8df3a9451f1
remind1990 <tigerstylest00@gmail.com>
2023-10-18 16:28:37 -0230

    changed picture

--- src/ui/customMarker.js
+++ src/ui/customMarker.js
@@ -13,7 +13,7 @@ import L from 'leaflet';
 // });
 
 const iconMarker = L.icon({
-  iconUrl: '../../public/pass.svg',
+  iconUrl: '../../public/travel.svg',
   iconSize: [40, 60], // size of the icon
   popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
 });
